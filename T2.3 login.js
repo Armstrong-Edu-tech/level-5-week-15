bc5f27faa81f057a855f240579841314bf5234ad
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-03-04 13:06:48 +0200

    Clean up login.js by removing unused code
    Removed commented-out login handler code.

--- T2.3 login.js
+++ T2.3 login.js
@@ -1,15 +1,4 @@
-// export default function handler(req, res) {
-// if(req.method==='POST'){
-//     const { email, password } = req.body;
 
-//      if (email === "test@test.com" && password === "123456") {
-//       return res.status(200).json({ message: "Login successful" });
-//     }
-
-//     return res.status(401).json({ message: "Invalid credentials" });
-// }
-// res.status(405).json({ message: "Method Not Allowed" });
-// }
 
 // pages/api/login.js
 import jwt from "jsonwebtoken";
