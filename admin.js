c5d3681634e6f3f3b7e732a2cb5c43a4677cc149
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-03-04 14:48:04 +0200

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,7 @@
+export default function Admin() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" ,color:"black",fontSize:"large"}}>
+      <h1>Admin Panel</h1>
+    </div>
+  );
+}
---
+++ dashboard.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" ,color:"black",fontSize:"large"}}>
+      <h1>User Dashboard</h1>
+    </div>
+  );
+}
