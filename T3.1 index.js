928dbd4453683e13bb624810b154d91f854aae47
Yara Megawer <108768001+yaramegawer@users.noreply.github.com>
2026-03-04 15:28:01 +0200

    Update T3.1 index.js

--- T3.1 index.js
+++ T3.1 index.js
@@ -1,23 +1,17 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import jwt from "jsonwebtoken";
-
-const SECRET = "mysecretkey"; // same as API
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem("token"); // get token from storage
-    if (!token) return;
+    // Example role
+    const userRole = "user"; // or "user"
 
-    try {
-      const decoded = jwt.verify(token, SECRET);
-      if (decoded.role === "admin") router.push("/admin");
-      else router.push("/dashboard");
-    } catch (err) {
-      console.log("Invalid or expired token");
-      localStorage.removeItem("token");
+    if (userRole === "admin") {
+      router.push("/admin");
+    } else if (userRole === "user") {
+      router.push("/dashboard");
     }
   }, []);
 
